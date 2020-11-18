d6e050d977b745cc0de1b90bda33aa8b09e6f2cb
liwenkang <changzhiliwenkang@outlook.com>
2020-11-18 11:04:09 +0800

    ci: change CI config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     port: 8080,
+    outputDir: 'dist',
+    publicPath: process.env.NODE_ENV === 'production' ? '/learn-vue3/' : '/',
     proxy: {
         '/api': {
             target: "http://47.100.38.254:5001/",
